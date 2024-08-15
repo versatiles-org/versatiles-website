6d7745372b1cdaa8c9eb18b378eb13d04642565b
Michael Kreil <github@michael-kreil.de>
2024-08-15 20:36:15 +0200

    add codeblock

---
+++ src/lib/components/CodeBlock.svelte
@@ -0,0 +1,80 @@
+<!-- CodeBlock.svelte -->
+<script lang="ts">
+	import Highlight from 'svelte-highlight';
+	import { bash, xml, json, javascript, typescript, css } from 'svelte-highlight/languages';
+	import github from 'svelte-highlight/styles/github-dark';
+
+	const languages = {
+		bash,
+		html: xml,
+		json,
+		js: javascript,
+		javascript,
+		ts: typescript,
+		typescript,
+		css
+	} as const;
+
+	type LanguageName = keyof typeof languages;
+	type LanguageType = (typeof languages)[LanguageName];
+
+	export let code = '';
+	export let languageName: LanguageName = 'bash';
+	let language: LanguageType = bash;
+	let copySuccess = false;
+
+	$: {
+		language = languages[languageName] || languages['bash']; // Fallback to bash
+	}
+
+	function copyToClipboard() {
+		navigator.clipboard.writeText(code).then(() => {
+			copySuccess = true;
+			setTimeout(() => (copySuccess = false), 2000); // Reset after 2 seconds
+		});
+	}
+</script>
+
+<svelte:head>
+	{@html github}
+</svelte:head>
+
+<div class="code-container">
+	<Highlight {language} {code} />
+	<button on:click={copyToClipboard}>
+		{#if copySuccess}
+			Copied!
+		{:else}
+			Copy
+		{/if}
+	</button>
+</div>
+
+<style>
+	.code-container {
+		position: relative;
+		margin: 0 auto 1em;
+		font-size: 0.7em;
+		width: 90%;
+		text-align: left;
+	}
+	button {
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 0.5em;
+		background: rgba(0, 0, 0, 0.5);
+		color: white;
+		border: none;
+		cursor: pointer;
+		border: 1px solid rgba(255, 255, 255, 0.2);
+		border-radius: 5px;
+		opacity: 0.5;
+		transition: opacity 0.2s ease;
+	}
+	button:hover {
+		background-color: rgba(255, 255, 255, 0.2);
+		border-color: rgba(255, 255, 255, 0.8);
+		opacity: 1;
+	}
+</style>
